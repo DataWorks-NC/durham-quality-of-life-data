42627717591e68eea7d2223602a53d6663d0b2f4
Tim Stallmann <tim@stallmann.mayfirst.org>
2018-04-11 16:26:16 -0400

    Change handling of grep command

--- .circleci/check-build-output-for-errors.sh
+++ .circleci/check-build-output-for-errors.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ grep -iq error ~/repo/npm-build-output.txt ]
+grep -i error ~/repo/npm-build-output.txt
+if [ $? -eq 0 ]
 then
-    exit 0
-else
-    grep -i error ~/repo/npm-build-output.txt
     exit 1
+else
+    exit 0
 fi
