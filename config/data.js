fb5e9e4b2e29ece1ab20de826942ef6db667ce2e
Allison Young <allisonyoung@Allisons-MacBook-Air.local>
2025-02-03 10:31:20 -0500

    update for 2022 and 2023 for ACS variable

--- config/data.js
+++ config/data.js
@@ -146,6 +146,7 @@ export default {
       y_2020: 49.46,
       y_2021: 50.72,
       y_2022: 51.88,
+      y_2023: 53.54,
     },
     'type': 'weighted',
     'geographies': [
@@ -174,6 +175,7 @@ export default {
       y_2020: 0.5,
       y_2021: 0.4,
       y_2022: 0.4,
+      y_2023: 0.4,
     },
     'type': 'weighted',
     'geographies': [
@@ -566,6 +568,7 @@ export default {
       y_2020: 75.4,
       y_2021: 71.4,
       y_2022: 67.8,
+      y_2023: 65.8,
     },
     'type': 'weighted',
     'geographies': [
@@ -945,6 +948,8 @@ export default {
       y_2019: 35.4,
       y_2020: 35.5,
       y_2021: 35.7,
+      y_2022: 35.8,
+      y_2023: 35.8,
     },
     'type': 'mean',
     'geographies': [
@@ -973,6 +978,8 @@ export default {
       y_2019: 1067,
       y_2020: 1103,
       y_2021: 1162,
+      y_2022: 1301,
+      y_2023: 1415,
     },
     'type': 'weighted',
     'geographies': [
@@ -1045,6 +1052,7 @@ export default {
       y_2020: 45261,
       y_2021: 46350,
       y_2022: 55970,
+      y_2023: 61726,
     },
     'type': 'weighted',
     'geographies': [
@@ -1073,6 +1081,8 @@ export default {
       y_2019: 75072,
       y_2020: 78067,
       y_2021: 84963,
+      y_2022: 93659,
+      y_2023: 100895,
     },
     'type': 'weighted',
     'geographies': [
@@ -1102,6 +1112,7 @@ export default {
       y_2020: 50306,
       y_2021: 51508,
       y_2022: 56936,
+      y_2023: 60145,
     },
     'type': 'weighted',
     'geographies': [
@@ -1131,6 +1142,7 @@ export default {
       y_2020: 36759,
       y_2021: 39602,
       y_2022: 43945,
+      y_2023: 46931,
     },
     'type': 'weighted',
     'geographies': [
@@ -1160,6 +1172,7 @@ export default {
       y_2020: 25.6,
       y_2021: 23.5,
       y_2022: 22.2,
+      y_2023: 22.0,
     },
     'geographies': [
       'tract',
@@ -1208,6 +1221,7 @@ export default {
       y_2020: 44.6,
       y_2021: 44.7,
       y_2022: 44.6,
+      y_2023: 45.0,
     },
     'type': 'weighted',
     'geographies': [
@@ -1237,6 +1251,8 @@ export default {
       y_2019: 3.9,
       y_2020: 3.6,
       y_2021: 3.5,
+      y_2022: 3.3,
+      y_2023: 3.3,
     },
     'type': 'weighted',
     'geographies': [
@@ -1279,6 +1295,8 @@ export default {
       y_2019: 311848,
       y_2020: 317665,
       y_2021: 320146,
+      y_2022: 325101,
+      y_2023: 329405,
     },
     'type': 'sum',
     'geographies': [
@@ -2105,6 +2123,7 @@ export default {
       y_2020: 21.0,
       y_2021: 20.9,
       y_2022: 20.9,
+      y_2023: 21.3,
     },
     'type': 'weighted',
     'geographies': [
@@ -2134,6 +2153,7 @@ export default {
       y_2020: 50.1,
       y_2021: 49.5,
       y_2022: 48.7,
+      y_2023: 49.6,
     },
     'type': 'weighted',
     'geographies': [
@@ -2255,6 +2275,7 @@ export default {
       y_2020: 9.4,
       y_2021: 14.2,
       y_2022: 17.7,
+      y_2023: 20.2,
     },
     'type': 'weighted',
     'geographies': [
@@ -2282,7 +2303,8 @@ export default {
       y_2019: 2.4,
       y_2020: 2.1,
       y_2021: 2.1,
-      Y_2022: 2.5,
+      y_2022: 2.5,
+      y_2023: 2.1,
     },
     'type': 'weighted',
     'geographies': [
